b7777af3d3b561494c0e8567ccc3c4fc9fce5815
Nils Lattek <n.lattek@geomobile.de>
2013-02-21 13:23:44 +0100

    Add finished column to project sort order.

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,7 +4,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = @projects.order('name')
+    @projects = @projects.order('finished, name')
 
     respond_to do |format|
       format.html # index.html.erb
